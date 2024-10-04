c3a4c43062503c6865712dca72e7c9d9b914d08c
blatifat <blatifat@student.42heilbronn.de>
2024-10-04 04:27:27 +0200

    CPP05

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: blatifat <blatifat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 02:57:17 by blatifat          #+#    #+#             */
-/*   Updated: 2024/10/04 03:07:03 by blatifat         ###   ########.fr       */
+/*   Updated: 2024/10/04 03:19:48 by blatifat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,23 @@
 int main() {
     try {
         Bureaucrat john("John", 2);
-        std::cout << john << std::endl;
-
+        std::cout << john << std::endl; 
         john.incrementGrade();
-        std::cout << "After increment: " << john << std::endl;
-
+        std::cout << "After increment: " << john << std::endl; 
         // Cette ligne devrait lancer une exception
         john.incrementGrade();
     } 
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
-
     try {
         Bureaucrat jane("Jane", 150);
         std::cout << jane << std::endl;
-
         // Cette ligne devrait lancer une exception
         jane.decrementGrade();
     } 
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
-
     return 0;
 }
